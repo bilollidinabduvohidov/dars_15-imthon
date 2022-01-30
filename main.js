e3e9162b79514a077f534600eb2d6a7fd27174ad
Abduvohidov Bilolliddin <bilol200414@gmail.com>
2022-01-31 02:36:33 +0500

    dashaboard index html

---
+++ main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('.border').on('click', function () {
+        $('.section').css('transform', 'translateX(-162px)')
+    })
+
+    $('.fas').on('click', function () {
+        $('.section').css('transform', 'translateX(1px)')
+    })
+
+})

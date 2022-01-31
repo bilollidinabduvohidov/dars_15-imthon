3fd9fb93fb60d56e4814fe819fcafe170f438aa9
Abduvohidov Bilolliddin <bilol200414@gmail.com>
2022-01-31 22:35:27 +0500

    js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('.border').on('click', function () {
-        $('.section').css('transform', 'translateX(-162px)')
+        $('.section').css('transform', 'translateX(-166px)')
     })
 
     $('.fas').on('click', function () {
@@ -8,3 +8,26 @@ $(document).ready(function() {
     })
 
 })
+
+Morris.Area({
+    element: 'area-example',
+    data: [
+      { y: '2006', a: 25},
+      { y: '2007', a: 50},
+      { y: '2008', a: 45},
+      { y: '2009', a: 75},
+      { y: '2010', a: 30},
+      { y: '2011', a: 55},
+      { y: '2012', a: 65},
+      { y: '2013', a: 60}
+    ],
+    xkey: 'y',
+    ykeys: ['a', ],
+    labels: ['Series A', 'Series B'],
+    lineColors: ['dodgerblue'],
+    pointFillColors:['black'],
+    pointSize: ['4px'],
+    fillOpacity: [0.3],
+  });
+
+  Morris.Donut({
